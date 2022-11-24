0713c188e32e384613c76dfda2df3b693c386f73
AleksanderSm <aleksander.smardzewski@gmail.com>
2022-11-24 20:00:47 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class Main {
+    public static void main(String[] args) {
+    Hero hero1 = new Hero("Janek","mag",2,22.5,true);
+        System.out.println();
+    }
+}
